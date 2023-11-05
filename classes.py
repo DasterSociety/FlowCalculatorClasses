cdc5ccb9a1838724454e3e9e0dcf02465a22f758
daster <124815565+DasterSociety@users.noreply.github.com>
2023-11-04 18:16:48 -0600

    Created the first file

---
+++ classes.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+
+class App(tk.Tk):
+    
+    def __init__(self)
+        super().__init__()
+        self.title("Droplet Generator Calculator")
+        self.geometry("600x600")
+        self.minsize(600,600)
+
+        self.mainloop()
+
+App()
